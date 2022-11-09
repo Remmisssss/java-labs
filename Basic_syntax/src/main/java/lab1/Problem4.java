66697be80ed186b29edbf270f2d090f4d063be06
Бабина Полина <117391977+Remmisssss@users.noreply.github.com>
2022-11-09 22:35:16 +0200

    Update Problem4.java

--- Basic_syntax/src/main/java/lab1/Problem4.java
+++ Basic_syntax/src/main/java/lab1/Problem4.java
@@ -6,11 +6,14 @@ public class Problem4 {
 
     public static int[] flattenMatrix(int[][] matrix) {
 
+        if (matrix.length == 0) return null;
+       
         int []arr = new int[matrix.length*matrix[0].length];
         int index = 0;
+
         for(int i=0;i<matrix[0].length;i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                arr[index] = matrix[j][i];
+            for (int[] ints : matrix) {
+                arr[index] = ints[i];
                 index++;
             }
         }
